09c63745a4af6abf4a2c3dab0ed7a7534afc60ef
Fedor Nikolaev <fsqcds@gmail.com>
2012-05-16 01:54:11 +0400

    Cool, it supports strings

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ module Main where
 
 import Derive
 
-data T = A Integer Integer Integer
+data T = A Integer String Integer
 $(deriveShow ''T "(@, @, @)")
 -- $(deriveRead ''T "(@, @, @)")
 
@@ -12,10 +12,10 @@ instance Read T where
   readsPrec _ s = [(A x1 x2 x3, r) | ("(", r1) <- lex s,
                                      (x1,  r2) <- reads r1 :: [(Integer, String)],
                                      (",", r3) <- lex r2,
-                                     (x2, r4) <- reads r3 :: [(Integer, String)],
+                                     (x2, r4) <- reads r3 :: [(String, String)],
                                      (",", r5) <- lex r4,
                                      (x3, r6) <- reads r5 :: [(Integer, String)],
                                      (")", r) <- lex r6]
 
-main = do print $ A 11 22 30
-          print $ (read "(23,   43,  822)" :: T)
+main = do print $ A 11 "foobar" 30
+          print $ (read "(23, \"quux\",  822)" :: T)
