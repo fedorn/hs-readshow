d6721f8b906c0d83c7a38fd763266680c8b134db
Fedor Nikolaev <fsqcds@gmail.com>
2012-05-16 01:48:04 +0400

    Trying to implement concrete reader

--- Main.hs
+++ Main.hs
@@ -5,7 +5,17 @@ module Main where
 import Derive
 
 data T = A Integer Integer Integer
-data T1 = B Integer Integer Integer deriving Show
 $(deriveShow ''T "(@, @, @)")
+-- $(deriveRead ''T "(@, @, @)")
 
-main = (print $ A 11 22 30) >> (print $ B 11 22 30)
+instance Read T where
+  readsPrec _ s = [(A x1 x2 x3, r) | ("(", r1) <- lex s,
+                                     (x1,  r2) <- reads r1 :: [(Integer, String)],
+                                     (",", r3) <- lex r2,
+                                     (x2, r4) <- reads r3 :: [(Integer, String)],
+                                     (",", r5) <- lex r4,
+                                     (x3, r6) <- reads r5 :: [(Integer, String)],
+                                     (")", r) <- lex r6]
+
+main = do print $ A 11 22 30
+          print $ (read "(23,   43,  822)" :: T)
